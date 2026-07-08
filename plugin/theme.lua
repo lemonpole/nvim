2ca5989a4c39627457452e60b5b9bf87969d20f1
lemonpole <srivera.x1@gmail.com>
2026-07-08 08:45:16 -0400

    feat: switch to gruvbox theme

--- plugin/theme.lua
+++ plugin/theme.lua
@@ -1,7 +1,5 @@
-vim.pack.add({ { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } })
+vim.pack.add({ "https://github.com/ellisonleao/gruvbox.nvim" })
 
-require("catppuccin").setup({
-  flavour = "mocha",
-})
+require("gruvbox").setup()
 
-vim.cmd.colorscheme("catppuccin-nvim")
+vim.cmd.colorscheme("gruvbox")
