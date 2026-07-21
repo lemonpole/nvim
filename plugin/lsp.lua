c0c1a612190987b2ab246637a1d50174bd6e6ad9
lemonpole <srivera.x1@gmail.com>
2026-07-21 14:16:25 -0400

    fix: fix eslint lsp path discrepancy on msys2

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -26,3 +26,30 @@ vim.lsp.config["clangd"] = {
     compilationDatabasePath = "build/Debug",
   },
 }
+
+--[[
+  fixes an issue with eslint not resolving path
+  aliases correctly on windows under msys2.
+
+  ```bash
+  # before fix
+  C:/msys64/path/to/project
+
+  # after fix
+  file:///C:/msys64/path/to/project
+  ```
+]]
+if vim.fn.has("win32") == 1 and vim.fn.exists("$MSYSTEM") == 1 then
+  vim.lsp.config("eslint", {
+    before_init = function(_, config)
+      if config.root_dir then
+        local root = vim.fs.normalize(config.root_dir)
+        config.settings = config.settings or {}
+        config.settings.workspaceFolder = {
+          uri = vim.uri_from_fname(root),
+          name = vim.fs.basename(root),
+        }
+      end
+    end,
+  })
+end
