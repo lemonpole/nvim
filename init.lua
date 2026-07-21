de515ca4fd7e30bb86b562d588e6a286f05f7070
lemonpole <srivera.x1@gmail.com>
2026-07-21 18:30:08 -0400

    feat: add nohlsearch package

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("options")
 require("keymaps")
 require("autocmds")
+
+-- PLUGINS
+--
+-- See `:h :packadd`, `:h vim.pack`
+
+-- Add the "nohlsearch" package to automatically disable search highlighting after
+-- 'updatetime' and when going to insert mode.
+vim.cmd("packadd! nohlsearch")
