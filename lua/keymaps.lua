6bc0256ce10b507cca935c5b9ef2607b141936a0
lemonpole <srivera.x1@gmail.com>
2026-07-21 14:18:50 -0400

    feat: add fzf live grep keybind

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>")
 vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>")
 vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua lsp_document_symbols<cr>")
+vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>")
 
 -- neogit - tui for git
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>")
